d1ee8b4af0060a8849692fbb716bb7dce7cad123
Tayo Ososanya <ososanyatayo@gmail.com>
2020-12-13 19:07:45 +0000

    test 20

--- beanft/app.py
+++ beanft/app.py
@@ -11,25 +11,33 @@ app= Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def hello():
-    return jsonify("hello from ML API of Titanic data!")
+    return jsonify("Hello from Bean Forecast App!")
 
-@app.route("/predictions", methods=["GET"])
+
+@app.route("/predictions" , methods=['GET'])
 def predictions():
+
     data = request.get_json()
     df=pd.DataFrame(data['data'])
+    # define explanatory vars
+    cols=['sepal_length','sepal_width','petal_length','petal_width']
+
     data_all_x_cols = cols
     try:
-        preprocessed_df=preprocess(df)
+        # preprocess the data for ML
+        new_data = df.dropna() # deletes Na and NaN
+        preprocessed_df = new_data
     except:
         return jsonify("Error occured while preprocessing your data for our model!")
-    filename=model_name
-    loaded_model = pickle.load(open(filename, 'rb'))
+    # filename = model_to_fit
+    saved_model = os.path.join(folder, str("final_model.sav")) 
+    loaded_model = pickle.load(open(saved_model, 'rb'))
     try:
         predictions= loaded_model.predict(preprocessed_df[data_all_x_cols])
     except:
         return jsonify("Error occured while processing your data into our model!")
     print("done")
-    response={'data':[],'prediction_label':{'survived':1,'not survived':0}}
+    response={'data':[]}
     response['data']=list(predictions)
     return make_response(jsonify(response),200)
 
